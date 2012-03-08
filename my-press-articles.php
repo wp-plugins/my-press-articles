32806a3e5422dc080ef217d435badeaa3850499c
GadgetsChoose <GadgetsChoose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-08 10:02:44 +0000

    Committing version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/my-press-articles/trunk@516224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-press-articles.php
+++ my-press-articles.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: my press articles
-Plugin URI: http://gadgets-code.com/tutorial-1-my-press-articles-how-to-embed-a-google-map-on-your-wordpress-post/
+Plugin URI: http://gadgets-code.com/my-press-articles-tutorial-5-embed-a-google-map-with-clickable-marker
 Description: Use shortcodes in your post to render Google Map, Twitter, Facebook and Youtube related services.
-Version: 1.3
+Version: 1.4
 Author: Gadgets Choose
 Author URI: http://wordpress.org/extend/plugins/my-press-articles/
 License: GPLv2
@@ -120,6 +120,89 @@ function googleMAP($atts) {
 }
 add_shortcode("googleMAP","googleMAP");
 
+//short code for google map overlay
+
+function googleMapOverlay($atts) {
+
+  $map_id_array = array();
+  extract(shortcode_atts(array("address"=>'',"width"=>'300',"height"=>'300',"zoom"=>'13', "mapid"=>'newmap'),$atts));
+
+  if (in_array($mapid, $map_id_array)) {
+     return '';
+   } else {
+     array_push($map_id_array, $mapid);
+     $address = esc_js($address);
+     $mapid = esc_js($mapid);
+     $width = esc_js($width);
+     $height = esc_js($height);
+     static $map_script = false;
+     $renderMap='';
+     if( $map_script == false ) {
+       $renderMap .= '<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>';
+       $map_script = true;
+     }
+     return $renderMap."<div id='$mapid' style='width:{$width}px;height:{$height}px;margin-top:3px;margin-bottom:2px;'></div>
+      <script>
+       function render_overlay_$mapid() {
+             var geocoder = new google.maps.Geocoder();
+             var address = \"$address\";
+             var map;
+             geocoder.geocode( { 'address': address }, function(results, status) {
+
+              if (status == google.maps.GeocoderStatus.OK) {
+                var latlng = results[0].geometry.location;
+                var options = {
+                  mapTypeId: google.maps.MapTypeId.HYBRID,
+                  center: latlng,
+                  zoom: $zoom
+                };
+                map = new google.maps.Map(document.getElementById('$mapid'), options);
+
+                var marker = new google.maps.Marker({
+                  position: latlng,
+                  map: map,
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                   googlemapOverlay();
+                });
+
+              }
+             });
+         }
+         google.maps.event.addDomListener(window, 'load', render_overlay_$mapid);
+       </script>";
+    }
+}
+add_shortcode("googleoverlay","googleMapOverlay");
+
+//render the google map overlay panel
+function show_map_overlay() {
+ global $post;
+ $whichevercode = get_post_meta($post->ID, 'presscode', true);
+
+ if (!empty($whichevercode) && is_single()) {
+  echo "<script>
+     jQuery(document).ready(function() {
+
+      jQuery('body').append('<div id=\"mypressback-map-ground\"></div>');
+      jQuery('body').append('<div id=\"mypress-panel\">".$whichevercode."<br/><a href=\"#\" id=\"close-panel\">close</a></div>');
+
+     });
+     </script>";
+
+ echo "<script>
+  function googlemapOverlay() {
+     jQuery(\"#mypressback-map-ground, #mypress-panel\").fadeIn(3300);
+     jQuery(\"#mypress-panel a#close-panel\").click(function() {
+      jQuery(\"#mypressback-map-ground, #mypress-panel\").fadeOut(2300);
+     });
+   }
+   </script>";
+
+ }
+}
+
 //short code to show and hide content based on referrer website
 
 function show_hide_content($attrs,$content=null) {
@@ -157,6 +240,21 @@ function gadget_dashboard_new() {
      echo '</div>';
 }
 
+//include stylesheet code can be found in http://codex.wordpress.org
+
+add_action('wp_enqueue_scripts', 'add_mypress_stylesheet');
+wp_enqueue_script( 'jquery');
+
+function add_mypress_stylesheet() {
+  $myStyleUrl = plugins_url('/css/styles.css', __FILE__); // Respects SSL, styles.css is relative to the current file
+  $myStyleFile = WP_PLUGIN_DIR . '/my-press-articles/css/styles.css';
+  if ( file_exists($myStyleFile) ) {
+   wp_register_style('mypressStyleSheets', $myStyleUrl);
+   wp_enqueue_style( 'mypressStyleSheets');
+  }
+}
+
+add_action('wp_footer','show_map_overlay');
 add_filter( 'comment_text', 'do_shortcode' );
 add_filter('widget_text', 'shortcode_unautop');
 add_filter('widget_text', 'do_shortcode');
